bc06045c1c77deb5f6aae55beccbb0a7c77e0f40
edwin-front-end <39464555+edwin-front-end@users.noreply.github.com>
2021-05-31 17:08:23 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,41 @@
+# Landing Page Project
+
+## Info
+
+Simple **Landing page** with a dynamic navigation bar.
+
+## Features
+
+### Dynamic navigation menu
+
+The navigation menu is created dynamically based on the sections of the page. Implemented also the functionality to scroll to the desired section.
+
+`const NavBuilder = () => {};`
+
+### Functionality that distinguish the section in view
+
+Sections are distinguished while the user is scrolling through the sections using the Javascript API `IntersectionObserver()`
+
+## License
+
+**MIT License**
+
+Copyright (c) [2021] [Edwin Mensah]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ js/app.js
@@ -0,0 +1,104 @@
+/**
+ *
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ *
+ * Dependencies: None
+ *
+ * JS Version: ES2015/ES6
+ *
+ * JS Standard: ESlint
+ *
+ */
+
+/**
+ * Define Global Variables
+ *
+ */
+const navBar = document.getElementById("navbar__list");
+const sections = document.querySelectorAll("section");
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ *
+ */
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ *
+ */
+
+// build the nav
+
+// looping over the sections to create "li" element
+const NavBuilder = () => {
+	sections.forEach((section) => {
+		const listItem = document.createElement("li");
+		listItem.classList.add("list-item")
+		// create an anchor tag for the menu links
+		listItem.innerHTML = `<a href="" class="menu__link">${section.dataset.nav}</a>`;
+		// listen to the click event on "li" element to scroll to the selected section
+		listItem.addEventListener("click", (e) => {
+			e.preventDefault();
+			section.scrollIntoView({
+				behavior: "smooth",
+				block: "end",
+				inline: "nearest",
+			});
+		});
+		// appending "li" to the "ul"
+		navBar.appendChild(listItem);
+	});
+};
+
+NavBuilder();
+
+// Add class 'active' to section when near top of viewport
+const navBarLinks = document.querySelectorAll("a.menu__link");
+console.log(navBarLinks)
+
+const sectionObserver = () => {
+	// Intersection Observer API used to observe which section is on the viewport
+	const options = {
+		root: null,
+		threshold: 0.35,
+		rootMargin: "0px",
+	};
+
+	const observer = new IntersectionObserver((entries) => {
+		entries.forEach((entry) => {
+			if (!entry.isIntersecting) {
+				entry.target.classList.remove("your-active-class");
+				// navBarLinks.forEach((link) => {
+				// 	link.entry.classList.remove("active");
+				// });
+			} else {
+				entry.target.classList.add("your-active-class");
+				navBarLinks.forEach((link)=>{link.classList.add("active")})
+			}
+		});
+	}, options);
+
+	sections.forEach((section) => {
+		observer.observe(section);
+	});
+};
+
+sectionObserver();
+// Scroll to anchor ID using scrollTO event
+
+/**
+ * End Main Functions
+ * Begin Events
+ *
+ */
+
+// Build menu
+
+// Scroll to section on link click
+
+// Set sections as active
